0c354399ea9a608007c89c8de1fae67fb3c343f4
Oscar22110 <63084473+Oscar22110@users.noreply.github.com>
2020-06-01 15:05:05 -0500

    Create comunidad.kt

---
+++ comunidad.kt
@@ -0,0 +1,14 @@
+package com.example.proyect
+
+class comunidad (clave: String, nomComu: String, guardian: String){
+    var clav: String =""
+    var nomcomu: String=""
+    var guardi: String=""
+
+    init {
+        this.clav=clave
+        this.nomcomu=nomComu
+        this.guardi=guardian
+
+    }
+}
