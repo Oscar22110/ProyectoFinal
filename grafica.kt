704b9d7a5ce122870e769eb145cfa4c6fa91b3c9
Oscar22110 <63084473+Oscar22110@users.noreply.github.com>
2020-06-01 15:06:53 -0500

    Create grafica.kt

---
+++ grafica.kt
@@ -0,0 +1,16 @@
+package com.example.proyect
+
+class grafica (idDbi: Int, antenaGragic: String, dbi: Double){
+    var id: Int = 0
+    var nom: String = ""
+    var db: Double = 0.0
+
+    init {
+        this.id = idDbi
+        this.nom = antenaGragic
+        this.db = dbi
+    }
+}
+
+
+
