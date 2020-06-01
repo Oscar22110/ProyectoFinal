cf9abe3c8223bd8537f51b9939b65fcb3bdc6483
Oscar22110 <63084473+Oscar22110@users.noreply.github.com>
2020-06-01 15:03:54 -0500

    Create antena.kt

---
+++ antena.kt
@@ -0,0 +1,15 @@
+package com.example.proyect
+
+class antena (idantena: Int, nomAnt: String, modelo: String, tipoAnt: String){
+    var idan: Int = 0
+    var nomant: String = ""
+    var model: String = ""
+    var tipo: String = ""
+
+    init {
+        this.idan = idantena
+        this.nomant = nomAnt
+        this.model = modelo
+        this.tipo = tipoAnt
+    }
+}
