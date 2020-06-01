8c7720eae0e1200bc4540875f800eada29e540d0
Oscar22110 <63084473+Oscar22110@users.noreply.github.com>
2020-06-01 15:20:07 -0500

    Create usuario.kt

---
+++ usuario.kt
@@ -0,0 +1,15 @@
+package com.example.proyect
+
+class usuario (correoUsr: String, nomusr: String){
+    var corr: String =""
+    var nom: String = ""
+
+    init {
+        this.corr = correoUsr
+        this.nom = nomusr
+    }
+}
+
+
+
+
